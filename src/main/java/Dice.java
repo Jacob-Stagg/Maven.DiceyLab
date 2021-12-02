c57f3f0267b87e5bb5dad1d99560b87e14770504
Stagg Jacob <staggjacob@zipcodes-MacBook-Pro-2.local>
2021-12-02 16:41:52 -0500

    finished Dice class

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,51 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Dice {
+    Dice dice;
+    Integer toss;
+    Integer rollAmount;
 
+    public Dice(int numberOfRolls) {
+        this.dice = new Dice(numberOfRolls);
+        this.toss = dice.tossAndSum();
+        this.rollAmount = numberOfRolls;
+    }
 
+    private Integer tossAndSum() {
+        Integer value = null;
+
+        for (int i = 0; i < rollAmount; i++) {
+            value += throwValue();
+        }
+
+        return value;
+    }
+
+    public int throwValue() {
+        return ThreadLocalRandom.current().nextInt(1, 6);
+    }
+
+    public Dice getDice() {
+        return dice;
+    }
+
+    public void setDice(Dice dice) {
+        this.dice = dice;
+    }
+
+    public Integer getToss() {
+        return toss;
+    }
+
+    public void setToss(Integer toss) {
+        this.toss = toss;
+    }
+
+    public Integer getRollAmount() {
+        return rollAmount;
+    }
+
+    public void setRollAmount(Integer rollAmount) {
+        this.rollAmount = rollAmount;
+    }
 }
